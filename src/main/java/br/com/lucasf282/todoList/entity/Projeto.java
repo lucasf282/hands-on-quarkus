bd39a79dd74cd56184f3be8ffbe180549904c500
Lucas Farias <lucasfsiqueira282@gmail.com>
2023-08-18 22:32:35 -0300

    refact: encapsulando variaveis

--- src/main/java/br/com/lucasf282/todoList/entity/Projeto.java
+++ src/main/java/br/com/lucasf282/todoList/entity/Projeto.java
@@ -1,18 +1,21 @@
 package br.com.lucasf282.todoList.entity;
 
 import jakarta.persistence.*;
-
 import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "projeto")
 public class Projeto {
     @Id
     @GeneratedValue
-    public Long id;
-    public String nome;
-    public String descricao;
+    private Long id;
+    private String nome;
+    private String descricao;
 
     @OneToMany(mappedBy = "projeto")
-    public List<Tarefa> tarefas;
+    private List<Tarefa> tarefas;
 }
--- src/main/java/br/com/lucasf282/todoList/entity/Tarefa.java
+++ src/main/java/br/com/lucasf282/todoList/entity/Tarefa.java
@@ -2,20 +2,24 @@ package br.com.lucasf282.todoList.entity;
 
 import br.com.lucasf282.todoList.enums.StatusTarefa;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "tarefa")
 public class Tarefa {
     @Id
     @GeneratedValue
-    public Long id;
-    public String nome;
-    public String descricao;
+    private Long id;
+    private String nome;
+    private String descricao;
     @Enumerated(EnumType.ORDINAL)
-    public StatusTarefa status;
+    private StatusTarefa status;
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "id", insertable=false, updatable=false)
-    public Projeto projeto;
+    @JoinColumn(name = "id_projeto", insertable=false, updatable=false)
+    private Projeto projeto;
 
 }
 
